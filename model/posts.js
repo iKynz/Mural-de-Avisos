c90572911345688bfa48b72fcaeb0b77c6ffaa87
iKynz <emanuell_messias369@hotmail.com>
2022-06-25 18:03:36 -0300

    first commit

---
+++ model/posts.js
@@ -0,0 +1,23 @@
+module.exports = {
+
+    posts: [
+        {
+            id: "over",
+            title: "Teste do Mural",
+            description: "Descrição teste"
+        },
+    ],
+
+    getAll() {
+        return this.posts;
+    },
+
+    newPost(title, description) {
+        this.posts.push({id: generateID(), title, description });
+    },
+  
+}
+
+function generateID() {
+    return Math.random().toString(36).substring(2, 9)
+};
